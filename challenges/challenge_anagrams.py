c37fa87d3783461b397da1fec16fdb225c84adbe
Bruno Batista <brunobatista101@gmail.com>
2021-02-04 18:02:02 -0300

    gabatiro

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,46 @@
+def merge_sort(array):
+    if len(array) <= 1:
+        return array
+
+    mid = len(array) // 2
+    left, right = merge_sort(array[:mid]), merge_sort(array[mid:])
+
+    return merge(left, right, array.copy())
+
+
+def merge(left, right, merged):
+    left_cursor, right_cursor = 0, 0
+
+    while left_cursor < len(left) and right_cursor < len(right):
+        if left[left_cursor] <= right[right_cursor]:
+            merged[left_cursor + right_cursor] = left[left_cursor]
+            left_cursor += 1
+        else:
+            merged[left_cursor + right_cursor] = right[right_cursor]
+
+        for left_cursor in range(left_cursor, len(left)):
+            merged[left_cursor + right_cursor] = left[left_cursor]
+
+        for right_cursor in range(right_cursor, len(right)):
+            merged[left_cursor + right_cursor] = right[right_cursor]
+
+        return merged
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if len(first_string) != len(second_string):
+        return False
+
+    sorted_first_string = merge_sort([*first_string])
+    sorted_second_string = merge_sort([*second_string])
+
+    is_anagram = False
+
+    for index in range(len(sorted_first_string)):
+        if sorted_first_string[index] != sorted_second_string[index]:
+            is_anagram = False
+            break
+        else:
+            is_anagram = True
+
+    return is_anagram
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,8 @@
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+    count = 0
+
+    for start_index, start_time in enumerate(start_time):
+        if start_time <= target_time and end_time[start_index] >= target_time:
+            count += 1
+
+    return count
