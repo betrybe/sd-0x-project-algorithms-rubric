0c12866786928d776e1ba87ecf3fa37222f956ae
Vitor Buxbaum <vitor.buxbaum@betrybe.com>
2021-02-05 14:26:20 -0300

    Atendimento ao Requisito 3, com 100% dos testes

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,47 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+
+    if not (
+        (first_string and second_string)
+        and (len(first_string) == len(second_string))
+    ):
+        return False
+
+    first_array = list(first_string)
+    second_array = list(second_string)
+
+    first_array = merge_sort(first_array)
+    second_array = merge_sort(second_array)
+
+    return first_array == second_array
+
+
+def merge_sort(array):
+    if len(array) <= 1:
+        return array
+
+    mid = len(array) // 2
+
+    left, right = merge_sort(array[:mid]), merge_sort(array[mid:])
+    return merge(left, right, array.copy())
+
+
+def merge(left, right, merged):
+
+    left_cursor, right_cursor = 0, 0
+
+    while left_cursor < len(left) and right_cursor < len(right):
+
+        if left[left_cursor] <= right[right_cursor]:
+            merged[left_cursor + right_cursor] = left[left_cursor]
+            left_cursor += 1
+        else:
+            merged[left_cursor + right_cursor] = right[right_cursor]
+            right_cursor += 1
+
+    for left_cursor in range(left_cursor, len(left)):
+        merged[left_cursor + right_cursor] = left[left_cursor]
+
+    for right_cursor in range(right_cursor, len(right)):
+        merged[left_cursor + right_cursor] = right[right_cursor]
+
+    return merged
